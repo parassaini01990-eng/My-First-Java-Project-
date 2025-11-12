670918739ba229702cd4569e83679b26179d44f7
parassaini01990-eng <parassaini01990@gmail.com>
2025-11-12 17:00:07 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,39 @@
+package com.ecommerce.model;
+
+import java.util.Scanner;
+
+public class Product {
+	
+private int id;
+private String name;
+private double price;
+private String category;
+
+public Product(int id,String name,double price,String category) {
+	this.id=id;
+	this.name=name;
+	this.price=price;
+}
+
+public int getId() {
+	return id;
+}
+
+public String getName() {
+	return name;
+}
+
+public double getPrice() {
+	return price;
+} 
+
+public String getCategory() {
+	return category;
+}
+
+@Override
+public String toString() {
+	return id+" "+name+" Rs."+price;
+	
+}
+}
